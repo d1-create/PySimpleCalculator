a399e5595cb3a7058c9fe794557afd12751f88d6
Dirushan Uthayamoorthy <116972461+d1-create@users.noreply.github.com>
2024-07-23 20:13:22 +0100

    Finish main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import sys #import sys
-
+import simpleeval #replace python eval function with external library
 #import required modules from pyqt6
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
-
-#TODO answer button
 
 #app UI and functionality class
 class MainWindow(QWidget):
@@ -15,6 +13,7 @@ class MainWindow(QWidget):
         self.setWindowTitle("PySimpleCalculator")#set title
         self.setLayout(QVBoxLayout())#set default layout
         
+        self.SetupVars() #setup variables in another function
         self.SetupUI() #setup UI in another function
         self.SetupStyle() #setup styling in another function
         self.SetupFunc() #setup functionality in another function
@@ -49,9 +48,8 @@ class MainWindow(QWidget):
         self.btn_divide = QPushButton("÷")
         self.btn_multiply = QPushButton("x")
         self.btn_dot = QPushButton(".")
-        self.btn_pi = QPushButton("π")
-        self.btn_leftbr = QPushButton("(")
-        self.btn_rightbr = QPushButton(")")
+        self.btn_pi = QPushButton("x²")
+        self.btn_ans = QPushButton("Ans")
         
         #add buttons to container in order-ish
         self.container.layout().addWidget(self.btn_1,1,0)
@@ -72,8 +70,7 @@ class MainWindow(QWidget):
         self.container.layout().addWidget(self.btn_clear,0,0)
         self.container.layout().addWidget(self.btn_dot,4,1)
         self.container.layout().addWidget(self.btn_pi,4,2)
-        self.container.layout().addWidget(self.btn_leftbr,0,1)
-        self.container.layout().addWidget(self.btn_rightbr,0,2)
+        self.container.layout().addWidget(self.btn_ans,0,1,1,2)
         #add whole containers and boxes
         self.layout().addWidget(self.result_text)
         self.layout().addWidget(self.container)
@@ -90,22 +87,66 @@ class MainWindow(QWidget):
 
     def SetupVars(self):
         self.list_all = [] #list of all things in program
-        
-        self.answer = 0.00 #answer/result of last calculation
-        
-    #TODO add functionality to the program
-    ##
-    #how it will work
-    #a-function to add everything to result text box(in nearly every function start and end)
-    #b-function to add number to list(use update function)
-    #c-function to add function(*,/,-.+) to list(use update function)
-    #d-function to find out answer and put it in variable (use update function)
-    #e-function to clear the list and the answer 
-    ##
-    def SetupFunc(self):
-        pass
-    
+        self.full_string = "" #full list compact string
+        self.answer = None #answer/result of last calculation
 
+    def SetupFunc(self):
+        #basic number buttons
+        self.btn_0.clicked.connect(lambda:self.add_str("0"))
+        self.btn_1.clicked.connect(lambda:self.add_str("1"))
+        self.btn_2.clicked.connect(lambda:self.add_str("2"))
+        self.btn_3.clicked.connect(lambda:self.add_str("3"))
+        self.btn_4.clicked.connect(lambda:self.add_str("4"))
+        self.btn_5.clicked.connect(lambda:self.add_str("5"))
+        self.btn_6.clicked.connect(lambda:self.add_str("6"))
+        self.btn_7.clicked.connect(lambda:self.add_str("7"))
+        self.btn_8.clicked.connect(lambda:self.add_str("8"))
+        self.btn_9.clicked.connect(lambda:self.add_str("9"))
+        self.btn_dot.clicked.connect(lambda:self.add_str("."))
+        self.btn_pi.clicked.connect(lambda:self.add_str("**"))
+        #operators
+        self.btn_equals.clicked.connect(lambda:self.FindAnswer())
+        self.btn_plus.clicked.connect(lambda:self.add_str("+"))
+        self.btn_minus.clicked.connect(lambda:self.add_str("-"))
+        self.btn_divide.clicked.connect(lambda:self.add_str("/"))
+        self.btn_multiply.clicked.connect(lambda:self.add_str("*"))
+        #other buttons
+        self.btn_ans.clicked.connect(lambda:self.add_str(self.answer))
+        self.btn_clear.clicked.connect(lambda:self.Clear())
+
+    #update the text in the top box
+    def UpdateBox(self):
+        self.Make_String()
+        self.result_text.setText(self.full_string)
+
+    #make a string out of the list
+    def Make_String(self):
+        self.full_string = "".join(self.list_all)
+
+    #find the answer of the expression
+    def FindAnswer(self):
+        self.UpdateBox()
+        answer = simpleeval.simple_eval(self.full_string) #use external libary eval
+        self.answer = str(answer)
+        
+        self.list_all.clear()
+        self.UpdateBox()
+        self.result_text.setText(self.answer) #set the text as the answer
+
+    #clear all data/variables from the app
+    def Clear(self):
+        self.UpdateBox() 
+        self.answer = None #set answer to none
+        self.list_all.clear() #clear list
+        self.UpdateBox()
+
+    #add a letter as string to the list
+    def add_str(self,string:str):
+        self.UpdateBox()
+        self.list_all.append(str(string)) #append a string to the list
+        self.UpdateBox()
+    
+        
 #execute the application    
 app = QApplication(sys.argv)#make app class with arguements
 Window = MainWindow()#instantiate window class
